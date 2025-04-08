0012e806a358ed1f2fcb4e899107e45e21e42ed8
Reynaldo <reynaldo.ruiz@ucb.edu.bo>
2025-04-08 18:33:13 -0400

    primer commit

---
+++ src/test/java/com/segundoParcial/parcial/ParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segundoParcial.parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
